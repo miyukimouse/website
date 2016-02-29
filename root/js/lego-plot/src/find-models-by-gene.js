b88ff5d3500ed1f4d3a4f4131e71b5d3d9a02959
Sibyl <sibyl@wormbase.org>
2016-02-29 19:42:24 +0000

    reformat code #4625

--- root/js/lego-plot/src/find-models-by-gene.js
+++ root/js/lego-plot/src/find-models-by-gene.js
@@ -42,13 +42,13 @@ var noctua_edge = model.edge;
 
 // Helper: log.
 function ll(arg1){
-    console.log('[' + (new Date()).toJSON() + ']: ', arg1);
+  console.log('[' + (new Date()).toJSON() + ']: ', arg1);
 }
 
 // Helper: die.
 function _die(message){
-    console.error('ERROR [' + (new Date()).toJSON() + ']: ' + message);
-    process.exit(-1);
+  console.error('ERROR [' + (new Date()).toJSON() + ']: ' + message);
+  process.exit(-1);
 }
 
 ///
@@ -61,7 +61,7 @@ var argv = require('minimist')(process.argv.slice(2));
 // What gene product ID will we operate on?
 var gp_id = argv['g'] || argv['gene-product'];
 if( ! gp_id ){
-    _die('Option (g|gene-product) is required.');
+  _die('Option (g|gene-product) is required.');
 }
 
 ///
@@ -91,20 +91,20 @@ meta_manager.facets(['model']);
 // Register the search, and the callback.
 meta_manager.register('search', function(resp, man){
 
-    // Collect the model IDs.
-    var incoming_model_list = resp.facet_field('model');
-    var models_found = [];
-    if( us.isArray(incoming_model_list) ){
+  // Collect the model IDs.
+  var incoming_model_list = resp.facet_field('model');
+  var models_found = [];
+  if( us.isArray(incoming_model_list) ){
 	each(incoming_model_list, function(model_facet_pair){
-	    models_found.push(model_facet_pair[0]);
+	  models_found.push(model_facet_pair[0]);
 	});
-    }
+  }
 
-    // If there are collected IDs, batch get information about those
+  // If there are collected IDs, batch get information about those
     // models. Otherwise bail out here.
-    if( models_found.length === 0 ){
+  if( models_found.length === 0 ){
 	_die('Could find no models for: ' + gp_id);
-    }else{
+  }else{
 
 	///
 	/// This is the second part: now that we have a list of model
@@ -114,48 +114,48 @@ meta_manager.register('search', function(resp, man){
 	// Convert from model IDs to URIs (as found in the index).
 	var aunits_found = [];
 	each(models_found, function(mid){
-	    var muri = 'http://model.geneontology.org/' + mid.substr(-16);
-	    aunits_found.push(muri);
+	  var muri = 'http://model.geneontology.org/' + mid.substr(-16);
+	  aunits_found.push(muri);
 	});
 
-    	// For the sake of ease, we'll create a new manager.
+    // For the sake of ease, we'll create a new manager.
 	var model_engine = new node_engine(golr_response);
 	var model_manager =
 		new golr_manager(endpoint_location, gconf, model_engine,'async');
-    	model_manager.set_personality('noctua_meta_model');
-    	model_manager.set_facet_limit(0); // No longer want facets.
-    	model_manager.set_results_count(max_model_count);
-    	model_manager.set_targets(aunits_found, ['annotation_unit']);
+    model_manager.set_personality('noctua_meta_model');
+    model_manager.set_facet_limit(0); // No longer want facets.
+    model_manager.set_results_count(max_model_count);
+    model_manager.set_targets(aunits_found, ['annotation_unit']);
 
-    	// On successful search, display a little model info.
-    	model_manager.register('search', function(resp, man){
+    // On successful search, display a little model info.
+    model_manager.register('search', function(resp, man){
 
-            // Look at all the models we got back.
-            each(resp.documents(), function(doc){
+      // Look at all the models we got back.
+      each(resp.documents(), function(doc){
 
 		if( doc && doc['owl_blob_json'] ){
 
-		    // Grab model JSON and convert it into an operable
-		    // graph.
-		    var jobj = JSON.parse(doc['owl_blob_json']);
-    		    var graph = new noctua_graph();
-		    graph.load_data_basic(jobj);
+		  // Grab model JSON and convert it into an operable
+		  // graph.
+		  var jobj = JSON.parse(doc['owl_blob_json']);
+    	  var graph = new noctua_graph();
+		  graph.load_data_basic(jobj);
 
-		    // The final thing that we'll do.
-    		    ll('Model: ' + doc['annotation_unit']);
-		    if( graph.id() ){
-    			ll(' Graph id: ' + graph.id());
-        //  ll(graph);
-          ll(JSON.stringify(jobj, null,2));
-		    }
-    		    ll(' Node count: ' + graph.all_nodes().length);
+		  // The final thing that we'll do.
+    	  ll('Model: ' + doc['annotation_unit']);
+		  if( graph.id() ){
+    		ll(' Graph id: ' + graph.id());
+            //  ll(graph);
+            ll(JSON.stringify(jobj, null,2));
+		  }
+    	  ll(' Node count: ' + graph.all_nodes().length);
 		}
-    	    });
-    	});
+      });
+    });
 
-    	// Trigger second action.
-    	model_manager.search();
-    }
+    // Trigger second action.
+    model_manager.search();
+  }
 });
 
 // Trigger initial action.
