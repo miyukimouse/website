e4aae06548410345e019ebc3f8f3a320965c4290
Sibyl <sibyl@wormbase.org>
2016-02-29 21:02:13 +0000

    turn find-model-by-gene script into a function #4625

--- root/js/lego-plot/src/find-models-by-gene.js
+++ root/js/lego-plot/src/find-models-by-gene.js
@@ -51,112 +51,125 @@ function _die(message){
   process.exit(-1);
 }
 
-///
-/// Process command line arguments.
-///
+function get_model(gp_id, callback){
+  ///
+  /// First pass: get the models IDs from GOlr, if any.
+  ///
 
-// CLI handling.
-var argv = require('minimist')(process.argv.slice(2));
+  // Construct context and manager.
+  var gconf = new golr_conf.conf(amigo.data.golr);
+  var meta_engine = new node_engine(golr_response);
+  var meta_manager =
+      new golr_manager(endpoint_location, gconf, meta_engine, 'async');
 
-// What gene product ID will we operate on?
-var gp_id = argv['g'] || argv['gene-product'];
-if( ! gp_id ){
-  _die('Option (g|gene-product) is required.');
+  // Restrict our search to the subset we're interested in.
+  meta_manager.set_personality('model_annotation');
+  meta_manager.add_query_filter('document_category', 'model_annotation');
+
+  // We just want models that have this gp.
+  meta_manager.add_query_filter('enabled_by', gp_id);
+
+  // We're not interested in the returned items, just the facet listing
+  // of the models. In fact, we'll return unlimited facets, no "real"
+  // results, and limit ourself to the "model" facet.
+  meta_manager.set_facet_limit(-1);
+  meta_manager.set_results_count(0);
+  meta_manager.facets(['model']);
+
+  // Register the search, and the callback.
+  meta_manager.register('search', function(resp, man){
+
+    // Collect the model IDs.
+    var incoming_model_list = resp.facet_field('model');
+    var models_found = [];
+    if( us.isArray(incoming_model_list) ){
+      each(incoming_model_list, function(model_facet_pair){
+        models_found.push(model_facet_pair[0]);
+      });
+    }
+
+    // If there are collected IDs, batch get information about those
+    // models. Otherwise bail out here.
+    if( models_found.length === 0 ){
+      _die('Could find no models for: ' + gp_id);
+    }else{
+
+      ///
+      /// This is the second part: now that we have a list of model
+      /// IDs, we will get the model information available for them.
+      ///
+
+      // Convert from model IDs to URIs (as found in the index).
+      var aunits_found = [];
+      each(models_found, function(mid){
+        var muri = 'http://model.geneontology.org/' + mid.substr(-16);
+        aunits_found.push(muri);
+      });
+
+      // For the sake of ease, we'll create a new manager.
+      var model_engine = new node_engine(golr_response);
+      var model_manager =
+          new golr_manager(endpoint_location, gconf, model_engine,'async');
+      model_manager.set_personality('noctua_meta_model');
+      model_manager.set_facet_limit(0); // No longer want facets.
+      model_manager.set_results_count(max_model_count);
+      model_manager.set_targets(aunits_found, ['annotation_unit']);
+
+      // On successful search, display a little model info.
+      model_manager.register('search', function(resp, man){
+
+        // Look at all the models we got back.
+        each(resp.documents(), function(doc){
+
+          if( doc && doc['owl_blob_json'] ){
+
+            // Grab model JSON and convert it into an operable
+            // graph.
+            var jobj = JSON.parse(doc['owl_blob_json']);
+            var graph = new noctua_graph();
+            graph.load_data_basic(jobj);
+
+            // The final thing that we'll do.
+            ll('Model: ' + doc['annotation_unit']);
+            callback(graph);
+          }
+        });
+      });
+
+      // Trigger second action.
+      model_manager.search();
+    }
+  });
+
+  meta_manager.search();
+
 }
 
-///
-/// First pass: get the models IDs from GOlr, if any.
-///
+if(require.main === module) {
 
-// Construct context and manager.
-var gconf = new golr_conf.conf(amigo.data.golr);
-var meta_engine = new node_engine(golr_response);
-var meta_manager =
-	new golr_manager(endpoint_location, gconf, meta_engine, 'async');
+  ///
+  /// Process command line arguments.
+  ///
 
-// Restrict our search to the subset we're interested in.
-meta_manager.set_personality('model_annotation');
-meta_manager.add_query_filter('document_category', 'model_annotation');
+  // CLI handling.
+  var argv = require('minimist')(process.argv.slice(2));
 
-// We just want models that have this gp.
-meta_manager.add_query_filter('enabled_by', gp_id);
-
-// We're not interested in the returned items, just the facet listing
-// of the models. In fact, we'll return unlimited facets, no "real"
-// results, and limit ourself to the "model" facet.
-meta_manager.set_facet_limit(-1);
-meta_manager.set_results_count(0);
-meta_manager.facets(['model']);
-
-// Register the search, and the callback.
-meta_manager.register('search', function(resp, man){
-
-  // Collect the model IDs.
-  var incoming_model_list = resp.facet_field('model');
-  var models_found = [];
-  if( us.isArray(incoming_model_list) ){
-	each(incoming_model_list, function(model_facet_pair){
-	  models_found.push(model_facet_pair[0]);
-	});
-  }
-
-  // If there are collected IDs, batch get information about those
-    // models. Otherwise bail out here.
-  if( models_found.length === 0 ){
-	_die('Could find no models for: ' + gp_id);
+  // What gene product ID will we operate on?
+  var gp_id = argv['g'] || argv['gene-product'];
+  if( ! gp_id ){
+    _die('Option (g|gene-product) is required.');
   }else{
-
-	///
-	/// This is the second part: now that we have a list of model
-	/// IDs, we will get the model information available for them.
-	///
-
-	// Convert from model IDs to URIs (as found in the index).
-	var aunits_found = [];
-	each(models_found, function(mid){
-	  var muri = 'http://model.geneontology.org/' + mid.substr(-16);
-	  aunits_found.push(muri);
-	});
-
-    // For the sake of ease, we'll create a new manager.
-	var model_engine = new node_engine(golr_response);
-	var model_manager =
-		new golr_manager(endpoint_location, gconf, model_engine,'async');
-    model_manager.set_personality('noctua_meta_model');
-    model_manager.set_facet_limit(0); // No longer want facets.
-    model_manager.set_results_count(max_model_count);
-    model_manager.set_targets(aunits_found, ['annotation_unit']);
-
-    // On successful search, display a little model info.
-    model_manager.register('search', function(resp, man){
-
-      // Look at all the models we got back.
-      each(resp.documents(), function(doc){
-
-		if( doc && doc['owl_blob_json'] ){
-
-		  // Grab model JSON and convert it into an operable
-		  // graph.
-		  var jobj = JSON.parse(doc['owl_blob_json']);
-    	  var graph = new noctua_graph();
-		  graph.load_data_basic(jobj);
-
-		  // The final thing that we'll do.
-    	  ll('Model: ' + doc['annotation_unit']);
-		  if( graph.id() ){
-    		ll(' Graph id: ' + graph.id());
-            //  ll(graph);
-            ll(JSON.stringify(jobj, null,2));
-		  }
-    	  ll(' Node count: ' + graph.all_nodes().length);
-		}
-      });
+    // Trigger initial action.
+    //meta_manager.search();
+    get_model(gp_id, function(graph){
+      if( graph.id() ){
+        ll(' Graph id: ' + graph.id());
+        //  ll(graph);
+        ll(JSON.stringify(graph, null,2));
+      }
+      ll(' Node count: ' + graph.all_nodes().length);
     });
-
-    // Trigger second action.
-    model_manager.search();
   }
-});
+}
 
-// Trigger initial action.
-meta_manager.search();
+module.exports = get_model;
