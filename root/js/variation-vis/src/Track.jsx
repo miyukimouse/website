9d55e40aa40944ae99adb58b1d5567c87e099551
Sibyl <sibyl@wormbase.org>
2016-03-08 02:19:13 -0500

    display data within a track

--- root/js/variation-vis/src/Track.jsx
+++ root/js/variation-vis/src/Track.jsx
@@ -9,6 +9,11 @@ export default class Track extends React.Component {
 
   static propTypes = {
     index: React.PropTypes.number.isRequired,
+    data: React.PropTypes.arrayOf(React.PropTypes.shape({
+      xMin: React.PropTypes.number,
+      xMax: React.PropTypes.number,
+      tip: React.PropTypes.string
+    })),
     tip: React.PropTypes.string,
     width: React.PropTypes.number,
     height: React.PropTypes.number
@@ -25,7 +30,8 @@ export default class Track extends React.Component {
 
   static defaultProps = {
     width: 100,
-    height: 10
+    height: 10,
+    data: []
   }
 
   getVerticalPosition = () => {
@@ -33,12 +39,31 @@ export default class Track extends React.Component {
   }
 
   showTooltip = (event) => {
+
+    // Get point in global SVG space
+    function cursorPoint(evt){
+      // Create an SVGPoint for future math
+      const svg = evt.target.ownerSVGElement;
+      const pt = svg.createSVGPoint();
+      pt.x = evt.clientX;
+      pt.y = evt.clientY;
+      // return pt.matrixTransform(svg.getScreenCTM().inverse());
+      return {
+        x: 0,
+        y: 0
+      }
+    }
+
+    //const {x, y} = cursorPoint(event);
+
     this.setState((prevState, currProps) => {
       return {
         tooltipEventID: prevState.tooltipEventID + 1,
         tooltipPosition: {
-          x: this.props.width/2,
-          y: this.getVerticalPosition()
+          // x,
+          // y
+          x: this.props.width / 2,
+          y: this.getVerticalPosition() + 10
         },
         tooltipTarget: event.target
       };
@@ -71,16 +96,40 @@ export default class Track extends React.Component {
 
   // }
 
+
+
+  /* data series within a track */
+  renderData(){
+    return (
+      this.props.data.map((dat, index) => {
+        return (
+          <rect key={`data-rect-${index}`}
+            x={dat.xMin}
+            y={this.getVerticalPosition()}
+            width={dat.xMax - dat.xMin}
+            height={this.props.height}
+            tip={dat.tip}
+            fill="grey"/>)
+      })
+    )
+  }
+
+
   render() {
     console.log(this.state);
     return (
       <g className="track"
         onMouseOver={this.showTooltip}
         onMouseOut={this.hideTooltip}>
-      <rect
-        x="0"
-        y={this.getVerticalPosition()}
-        width={this.props.width} height={this.props.height} fill="grey" />
+
+        <rect
+          x="0"
+          y={this.getVerticalPosition()}
+          width={this.props.width} height={this.props.height} fill="lightgrey" />
+
+        {
+          this.renderData()
+        }
         {
           this.state.tooltipPosition ? <Tooltip {...this.state.tooltipPosition} tip={this.props.tip}/> : null
         }
--- root/js/variation-vis/src/main.jsx
+++ root/js/variation-vis/src/main.jsx
@@ -5,11 +5,24 @@ import Track from './Track.jsx';
 class App extends React.Component {
 
   render() {
+    const data1 = [
+      {
+        xMin: 5,
+        xMax: 15,
+        tip: 'exon 1'
+      },
+      {
+        xMin: 40,
+        xMax: 70,
+        tip: 'exon 2'
+      }
+    ];
     return (
       <div>
         <svg width="500px" height="200px"
           viewBox="0 0 100 40">
-          <Track index={0} tip="one track" />
+          <Track index={0} tip="one track"
+            data={data1}/>
           <Track index={1} tip="track number 2" />
         </svg>
       </div>
