716269bef6fa238700f33ea39c56ba51f2a0b43b
Sibyl <sibyl@wormbase.org>
2016-03-08 10:32:37 -0500

    button zoom

--- root/js/variation-vis/src/main.jsx
+++ root/js/variation-vis/src/main.jsx
@@ -2,7 +2,41 @@ import React from 'react';
 import { render } from 'react-dom';
 import Track from './Track.jsx';
 
+const DEFAULT_VISIBLE_WIDTH = 100;
+
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      center: DEFAULT_VISIBLE_WIDTH/2,
+      zoomFactor: 1,
+    };
+  }
+
+  createZoomHandler = (multiple, locatable) => {
+    return (event) => {
+      this.setState((prevState) => {
+        const newZoomFactor = prevState.zoomFactor * multiple;
+        const newCenter = locatable ? this.svgX(event.target.clientX) : this.state.center;
+        return {
+          center: newCenter,
+          zoomFactor: newZoomFactor < 1 ? 1 : newZoomFactor
+        }
+      })
+    }
+
+  }
+
+  _getVisibleWidth = () => {
+    return DEFAULT_VISIBLE_WIDTH / this.state.zoomFactor;
+  }
+
+  getViewBox = () => {
+    const visibleWidth = this._getVisibleWidth();
+    const x = Math.floor(this.state.center - visibleWidth/2)
+    return [x, 0, visibleWidth, 40].join(' ');
+  }
 
   render() {
     const data1 = [
@@ -19,8 +53,13 @@ class App extends React.Component {
     ];
     return (
       <div>
+        <div style={{margin: "20px"}}>
+          <button onClick={this.createZoomHandler(2)}>Zoom in (+)</button>
+          <button onClick={this.createZoomHandler(0.5)} style={{margin: "0 20px"}}>Zoom out (-)</button>
+        </div>
         <svg width="500px" height="200px"
-          viewBox="0 0 100 40">
+          viewBox={this.getViewBox()}
+          preserveAspectRatio="none meet">
           <Track index={0} tip="one track"
             data={data1}/>
           <Track index={1} tip="track number 2" />
