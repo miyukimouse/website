2efae600ef0734e2363a6e4ff8e628f2dc32099e
Sibyl <sibyl@wormbase.org>
2016-03-15 02:54:13 -0400

    set segment label by detecting intitial width and then divide by zoomFactor

--- root/js/variation-vis/src/components/DataSegment.jsx
+++ root/js/variation-vis/src/components/DataSegment.jsx
@@ -11,26 +11,75 @@ export default class Button extends React.Component {
     height: React.PropTypes.number
   }
 
-  componentDidMount() {
-    // const node = ReactDOM.findDOMNode(this);
-    // $(node).popup({
-    //   content: 'aaaaa'
-    // });
+  constructor(props) {
+    super(props);
+    this.state = {
+      labelWidthInitial: null
+    };
   }
 
-  componentDidUpdate() {
-    // const node = ReactDOM.findDOMNode(this);
-    // $(node).popup({
-    //   content: 'aaaaa'
-    // });
-    // const node = ReactDOM.findDOMNode(this);
-    // $(node).popup('refresh');
-   // $('.ui.dropdown').dropdown('refresh');
+  static contextTypes = {
+    zoomFactor: React.PropTypes.number
+  }
+
+  componentDidMount() {
+    const labelNode = this.refs.label;
+    const labelWidthInitial = labelNode.getBBox().width;
+
+    this.setState({
+      labelWidthInitial
+    });
+  }
+
+  // componentDidUpdate() {
+  // }
+
+  _getLabelVisibility() {
+    if (this.state.labelWidthInitial && this._getLabelScaledWidth() < this.props.width * 0.8){
+      return 'visible';
+    }else{
+      return 'hidden';
+    }
+  }
+
+  _getLabelScaledWidth() {
+    return this.state.labelWidthInitial / this.context.zoomFactor;
+  }
+
+  _getLabelCoords() {
+    const x = this.props.x + this.props.width / 2;
+    const y = this.props.y + 5;
+
+    if (this.state.labelWidthInitial) {
+      return {
+        x,
+        y,
+        textLength: this._getLabelScaledWidth()
+      }
+    } else {
+      return {
+        x,
+        y
+      }
+    }
   }
 
   render() {
 
     return (
-      <rect {...this.props}/>);
+      <g>
+        <rect {...this.props}/>
+        <text ref="label"
+              is="svg-text"
+              visibility={this._getLabelVisibility()}
+              text-anchor='middle'
+              font-size="4"
+              lengthAdjust="spacingAndGlyphs"
+              {...this._getLabelCoords()}>
+        {
+          this.props.tip
+        }
+        </text>
+      </g>);
   }
 };
