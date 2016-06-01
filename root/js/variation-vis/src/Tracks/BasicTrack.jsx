c2c07c4addafe199570c0dcb4e2f4c16eba8f2f0
Sibyl Gao <sibyl@wormbase.org>
2016-06-01 16:20:34 -0400

    mv SequenceComponent to components/folder

--- root/js/variation-vis/src/Tracks/BasicTrack.jsx
+++ root/js/variation-vis/src/Tracks/BasicTrack.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { findDOMNode } from 'react-dom';
 import Tooltip from '../Tooltip';
-import SequenceComponent from '../SequenceComponent';
+import SequenceComponent from '../components/SequenceComponent';
 import DataSegment from '../components/DataSegment';
 import $ from 'jquery';
 import { TRACK_HEIGHT } from '../Utils'
