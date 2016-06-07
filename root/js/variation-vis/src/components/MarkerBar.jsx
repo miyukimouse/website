59cbe820b19d77d65242e742a2e7ddba716fe573
Sibyl <sibyl@wormbase.org>
2016-06-07 16:42:28 -0400

    fix forgetting to checking the MarkerBar.jsx component

---
+++ root/js/variation-vis/src/components/MarkerBar.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import BasicTrack from '../Tracks';
+
+export default class MarkerBar extends React.Component {
+
+  static contextTypes = {
+    toWidth: React.PropTypes.func
+  };
+
+  static propTypes = {
+    cursorSVGCoordinate: React.PropTypes.number.isRequired,
+    // sequenceLength: React.PropTypes.number,
+    coordinateMapping: React.PropTypes.shape({
+      toSVGCoordinate: React.PropTypes.func,
+      toSequenceCoordinate: React.PropTypes.func
+    }).isRequired,
+    height: React.PropTypes.number
+  }
+
+  _cursorSequenceCoordinate() {
+    const {cursorSVGCoordinate, coordinateMapping} = this.props;
+    return Math.floor(coordinateMapping.toSequenceCoordinate(cursorSVGCoordinate));
+  }
+
+  _padSequneceCoordinates = (startSequenceCoord, endSequenceCoord) => {
+    if (typeof variable === 'undefined') {
+      endSequenceCoord = startSequenceCoord + 1;
+    }
+
+    const coordinateMapping = this.props.coordinateMapping;
+
+    const svgWidth = coordinateMapping.toSVGCoordinate(endSequenceCoord - startSequenceCoord);
+
+    // make the bar at least 1px width
+    const minApparentWidth = 1;
+    const minSvgWidth = this.context.toWidth(minApparentWidth);
+    if (svgWidth < minSvgWidth) {
+      const paddedSequenceLength = coordinateMapping.toSequenceCoordinate(minSvgWidth);
+      return {
+        start: startSequenceCoord,
+        end: startSequenceCoord + Math.ceil(paddedSequenceLength)
+      }
+    } else {
+      return {
+        start: startSequenceCoord,
+        end: endSequenceCoord
+      }
+    }
+  }
+
+  render() {
+    const barCoordinates = this._padSequneceCoordinates(this._cursorSequenceCoordinate())
+    return (<BasicTrack
+      data={[{
+        ...barCoordinates,
+        color: '#fd0',
+        fillOpacity: 0.5
+      }]}
+      coordinateMapping={this.props.coordinateMapping}
+      y={0}
+      height={this.props.height || 600}/>)
+  }
+}
