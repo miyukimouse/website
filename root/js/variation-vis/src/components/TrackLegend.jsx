955c40346c96d1b23c68e19bb247b2e795082a6c
Sibyl <sibyl@wormbase.org>
2016-06-28 18:49:41 -0400

    draw track legends

--- root/js/variation-vis/src/components/TrackLegend.jsx
+++ root/js/variation-vis/src/components/TrackLegend.jsx
@@ -1,29 +1,60 @@
 import React from 'react';
 import ColorScheme, { COLORS } from '../Utils/ColorHelper';
+import PrettyTrackSVGFilter from './PrettyTrackSVGFilter';
+
+const LEGEND_ITEM_OUTER_HEIGHT = 32;
+const LEGEND_ITEM_HEIGHT = 22;
+const LEGEND_ITEM_WIDTH = 22;
 
 export default class TrackLegend extends React.Component {
   static propTypes = {
     colorScheme: React.PropTypes.object,
   }
 
-  _renderLegendItem(dat) {
-    return (<li>
-      {dat.color} - {dat.group}
-    </li>)
+  _renderLegends() {
+    const colorScheme = this.props.colorScheme;
+    const legendData = colorScheme.getLegendData();
+    const style = {
+      fontFamily: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+      padding: 10
+    };
+    return (
+      colorScheme ? <svg
+        style={style}
+        height={legendData.length * LEGEND_ITEM_OUTER_HEIGHT}>
+        <defs>
+          <PrettyTrackSVGFilter/>
+        </defs>
+        {
+          legendData.map((dat, index) => {
+            return this._renderLegendItem(dat, index)
+          })
+        }
+      </svg>: null);
+  }
+
+  _renderLegendItem(dat, index) {
+    return (<g>
+      <rect
+        filter={`url(#${PrettyTrackSVGFilter.ID})`}
+        x={0}
+        y={index * LEGEND_ITEM_OUTER_HEIGHT}
+        width={LEGEND_ITEM_WIDTH}
+        height={LEGEND_ITEM_HEIGHT}
+        fill={dat.color}
+        stroke="#000000"/>
+      <text
+         x={LEGEND_ITEM_WIDTH * 2}
+         y={index * LEGEND_ITEM_OUTER_HEIGHT + 0.55 * LEGEND_ITEM_OUTER_HEIGHT}>
+      {
+        dat.group
+      }
+      </text>
+
+    </g>)
   }
 
   render() {
-    const colorScheme = this.props.colorScheme;
-    return (
-      colorScheme ? <div>
-      <ul>
-      {
-        colorScheme.getLegendData().map((dat) => {
-          return this._renderLegendItem(dat)
-        })
-      }
-      </ul>
-      </div> : null
-    );
+    return this._renderLegends();
   }
 }
